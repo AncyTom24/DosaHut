ca68e183f5476e8a865de53ff1c82b5b92019713
Ancy Tom <ancytom24@gmail.com>
2026-06-08 13:51:33 -0400

    Temp disable Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,1 @@
-// 🔥 PASTE YOUR FIREBASE CONFIG HERE
-// Get it from: Firebase Console → Project Settings → Your Apps → Web App
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDJdW6sPAZnMdAh47fb73P7xBXQijCuXeo",
-  authDomain: "dosa-pos.firebaseapp.com",
-  databaseURL: "https://dosa-pos-default-rtdb.firebaseio.com",
-  projectId: "dosa-pos",
-  storageBucket: "dosa-pos.firebasestorage.app",
-  messagingSenderId: "779561478371",
-  appId: "1:779561478371:web:549e7409e67d1b06fbbdd6",
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+export const db = null;
